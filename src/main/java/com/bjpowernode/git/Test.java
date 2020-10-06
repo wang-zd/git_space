96937be8142aa9b63ec5077312892a41a7d7f813
lisi <lisi@163.com>
2020-10-06 11:01:21 +0800

    创建工程git-test

---
+++ src/main/java/com/bjpowernode/git/Test.java
@@ -0,0 +1,11 @@
+package com.bjpowernode.git;
+
+/**
+ * @Author 王振铎
+ * @Date 2020/10/6 10:36
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello，world！");
+    }
+}
