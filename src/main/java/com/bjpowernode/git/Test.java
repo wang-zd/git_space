dc736b373d3691789c64b8d4290c8e4c3ef8ea2d
lisi <lisi@163.com>
2020-10-07 09:06:11 +0800

    添加了一行hhhhh！！！！！

--- src/main/java/com/bjpowernode/git/Test.java
+++ src/main/java/com/bjpowernode/git/Test.java
@@ -7,5 +7,6 @@ package com.bjpowernode.git;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello，world！");
+        System.out.println("hhhhhh！！！！");
     }
 }
